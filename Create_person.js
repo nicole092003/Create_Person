c6066b7a5d37d2fc1c8e24acdaf350c663518b57
Dorita Nicole Thomas <95882149+nicole092003@users.noreply.github.com>
2022-03-29 15:16:11 -0400

    Add files via upload

---
+++ Create_person.js
@@ -0,0 +1,11 @@
+
+Box = (GetElementByID) ('id'); {
+function Person(firstName, lastName);
+this.firstName = firstName;
+this.lastName = lastName;
+
+this.fullName = function() {
+    return this.firstName + "" + this.lastName;
+}
+
+}
